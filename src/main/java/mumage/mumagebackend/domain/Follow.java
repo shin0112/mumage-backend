256dc80acabf0bd5d2d5658dd2cae4416853be3a
leeeunda <132417166+leeeunda@users.noreply.github.com>
2023-11-21 00:59:56 +0900

    Feature follow - 팔로우 & 언팔로우 기능 & 팔로우/팔로잉 리스트 조회 (#9)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ src/main/java/mumage/mumagebackend/domain/Follow.java
@@ -0,0 +1,33 @@
+package mumage.mumagebackend.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Follow {
+
+    @Id @GeneratedValue
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_user")
+    private User from; // 누가 팔로우 하는가
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="to_user")
+    private User to; // 누구를 팔로우 하는가
+
+    @Builder
+    Follow(User from, User to){
+        this.from = from;
+        this.to = to;
+    }
+
+}
