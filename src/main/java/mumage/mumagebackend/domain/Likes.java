e8c6e71cf9589628bd48ee6c92a2a2bf07bac5c8
shin0112 <83761128+shin0112@users.noreply.github.com>
2023-11-21 02:14:56 +0900

    Fix urgent comments (#16)
    * fix: codes to comments
    
    * fix: codes to comments

--- src/main/java/mumage/mumagebackend/domain/Likes.java
+++ src/main/java/mumage/mumagebackend/domain/Likes.java
@@ -1,32 +1,32 @@
-package mumage.mumagebackend.domain;
-
-import jakarta.persistence.Entity;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import jakarta.persistence.*;
-
-@Entity
-@Getter
-@NoArgsConstructor
-public class Likes {
-
-    @Id @GeneratedValue
-    @Column(name = "likes_id")
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "posts_id")
-    private Posts posts; // 좋아요 누른 게시글
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "User_id")
-    private User user; // 좋아요 누른 사용자
-
-    @Builder
-    public Likes(Posts posts, User user){
-        this.posts = posts;
-        this.user = user;
-    }
-}
+//package mumage.mumagebackend.domain;
+//
+//import jakarta.persistence.Entity;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class Likes {
+//
+//    @Id @GeneratedValue
+//    @Column(name = "likes_id")
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "posts_id")
+//    private Posts posts; // 좋아요 누른 게시글
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "User_id")
+//    private User user; // 좋아요 누른 사용자
+//
+//    @Builder
+//    public Likes(Posts posts, User user){
+//        this.posts = posts;
+//        this.user = user;
+//    }
+//}
