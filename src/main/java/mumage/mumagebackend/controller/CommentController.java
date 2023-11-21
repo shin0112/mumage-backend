957fa49f8860358208aee4a9eaedb9103d6efc4e
shin0112 <83761128+shin0112@users.noreply.github.com>
2023-11-21 13:33:33 +0900

    Revert "Fix urgent comments (#17)"
    This reverts commit 82c35c2b9c6664dd85dfc3bb37754aad83daba5a.

--- src/main/java/mumage/mumagebackend/controller/CommentController.java
+++ src/main/java/mumage/mumagebackend/controller/CommentController.java
@@ -1,40 +1,40 @@
-package mumage.mumagebackend.controller;
-
-import lombok.RequiredArgsConstructor;
-import mumage.mumagebackend.dto.CommentDto;
-import mumage.mumagebackend.service.CommentsService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-@RequiredArgsConstructor
-@Controller
-public class CommentController {
-
-    private final CommentsService commentsService;
-
-    // 댓글 등록
-    @PostMapping("/comment/write/{postId}&{userId}")
-    public String commentWritePost(@PathVariable("postId") Long postId,
-                                   @PathVariable("userId") Long userId,
-                                   @ModelAttribute("commentDto") CommentDto commentDto){
-        commentsService.save(postId,userId,commentDto);
-        return "redirect:/post/read/{postId}";
-    }
-
-    // 댓글 삭제
-    @GetMapping("/comment/delete/{commentId}&{postId}")
-    public String commentDeleteGet(@PathVariable("commentId") Long commentId,
-                                   @PathVariable("postId") Long postId){
-        commentsService.delete(commentId);
-        return "redirect:/post/read/{postId}";
-    }
-
-    // 댓글 삭제 실패 : 댓글 작성자와 사용자가 다른 경우
-    @GetMapping("/comment/noAuthority")
-    public String notDeleteGet(){
-        return "comment/noAuthority";
-    }
-}
+//package mumage.mumagebackend.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import mumage.mumagebackend.dto.CommentDto;
+//import mumage.mumagebackend.service.CommentsService;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class CommentController {
+//
+//    private final CommentsService commentsService;
+//
+//    // 댓글 등록
+//    @PostMapping("/comment/write/{postId}&{userId}")
+//    public String commentWritePost(@PathVariable("postId") Long postId,
+//                                   @PathVariable("userId") Long userId,
+//                                   @ModelAttribute("commentDto") CommentDto commentDto){
+//        commentsService.save(postId,userId,commentDto);
+//        return "redirect:/post/read/{postId}";
+//    }
+//
+//    // 댓글 삭제
+//    @GetMapping("/comment/delete/{commentId}&{postId}")
+//    public String commentDeleteGet(@PathVariable("commentId") Long commentId,
+//                                   @PathVariable("postId") Long postId){
+//        commentsService.delete(commentId);
+//        return "redirect:/post/read/{postId}";
+//    }
+//
+//    // 댓글 삭제 실패 : 댓글 작성자와 사용자가 다른 경우
+//    @GetMapping("/comment/noAuthority")
+//    public String notDeleteGet(){
+//        return "comment/noAuthority";
+//    }
+//}
--- src/main/java/mumage/mumagebackend/repository/LikesRepository.java
+++ src/main/java/mumage/mumagebackend/repository/LikesRepository.java
@@ -1,18 +1,18 @@
-package mumage.mumagebackend.repository;
-
-
-import mumage.mumagebackend.domain.Likes;
-import mumage.mumagebackend.domain.User;
-import mumage.mumagebackend.domain.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.Set;
-
-@Repository
-public interface LikesRepository extends JpaRepository<Likes, Long> {
-
-    Optional<Likes> findByPostsAndUser(Posts posts, User user); // 게시글과 작성자로 좋아요 반환
-    Set<Likes> findByPosts(Posts posts); // 게시글에 누른 좋아요 목록 반환
-}
+//package mumage.mumagebackend.repository;
+//
+//
+//import mumage.mumagebackend.domain.Likes;
+//import mumage.mumagebackend.domain.User;
+//import mumage.mumagebackend.domain.Posts;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//import java.util.Set;
+//
+//@Repository
+//public interface LikesRepository extends JpaRepository<Likes, Long> {
+//
+//    Optional<Likes> findByPostsAndUser(Posts posts, User user); // 게시글과 작성자로 좋아요 반환
+//    Set<Likes> findByPosts(Posts posts); // 게시글에 누른 좋아요 목록 반환
+//}
