96c9939dfb51b7947fafef9bcadb762aa1d93645
shin0112 <wnslcosltimo12@gmail.com>
2024-01-31 12:55:47 +0900

    [refactor] add import domain.Role

--- src/main/java/mumage/mumagebackend/dto/UserJoinDto.java
+++ src/main/java/mumage/mumagebackend/dto/UserJoinDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import mumage.mumagebackend.domain.Role;
 
 @Getter
 @Setter
